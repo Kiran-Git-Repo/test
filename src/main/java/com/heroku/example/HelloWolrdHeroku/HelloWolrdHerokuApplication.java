804b35d290a9a43173d3f51d52563af090412820
Kiran-Git-Repo <85741699+Kiran-Git-Repo@users.noreply.github.com>
2021-06-20 12:55:11 +0530

    updating imports

--- src/main/java/com/heroku/example/HelloWolrdHeroku/HelloWolrdHerokuApplication.java
+++ src/main/java/com/heroku/example/HelloWolrdHeroku/HelloWolrdHerokuApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @RestController
